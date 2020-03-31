f7fe00b4911205438be3a7d792310622bcb376ef
Vladyslav Karpenko <klarpen@gmail.com>
2020-03-31 19:19:14 +0300

    Remove standart widgets

--- klarpen-wp-cleanup.php
+++ klarpen-wp-cleanup.php
@@ -24,7 +24,7 @@ function klrpn_exclude_unused_scripts_n_styles(){
 # Comment the action if not used
 add_action( 'init', 'klrpn_exclude_unused_scripts_n_styles' );
 
-#Completely switch off the Emoji: START
+# Completely switch off the Emoji: START
 add_filter('emoji_svg_url', '__return_empty_string');
 remove_action('wp_head', 'print_emoji_detection_script', 7);
 remove_action('admin_print_scripts', 'print_emoji_detection_script');
@@ -42,4 +42,26 @@ function wph_remove_emojis_tinymce($plugins) {
 }
 add_filter('tiny_mce_plugins', 'wph_remove_emojis_tinymce');
 add_filter('option_use_smilies', '__return_false');
-#Completely switch off the Emoji: END
+# Completely switch off the Emoji: END
+
+# Remove standart widgets
+add_action( 'widgets_init', 'klrpn_unregister_basic_widgets' );
+function klrpn_unregister_basic_widgets() {
+	unregister_widget('WP_Widget_Pages');
+	unregister_widget('WP_Widget_Calendar');
+	// unregister_widget('WP_Widget_Archives');
+	// unregister_widget('WP_Widget_Links');
+	// unregister_widget('WP_Widget_Meta');
+	// unregister_widget('WP_Widget_Search');
+	// unregister_widget('WP_Widget_Text');
+	// unregister_widget('WP_Widget_Categories');
+	// unregister_widget('WP_Widget_Recent_Posts');
+	unregister_widget('WP_Widget_Recent_Comments');
+	unregister_widget('WP_Widget_RSS');
+	// unregister_widget('WP_Widget_Tag_Cloud');
+	// unregister_widget('WP_Nav_Menu_Widget'); 
+	// unregister_widget('WP_Widget_Media_Audio');
+	// unregister_widget('WP_Widget_Media_Video');
+	// unregister_widget('WP_Widget_Media_Gallery');
+	// unregister_widget('WP_Widget_Media_Image');
+}
